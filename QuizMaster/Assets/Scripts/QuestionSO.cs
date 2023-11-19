fb96cd118c9f0556c7ef3e7f312fa146eb682f26
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-11-20 01:32:28 +0900

    section_4_55_complete
    new question add
    and new AssetMenu create

---
+++ QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    // 최소 2자리, 최대 6자리
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new Question text here";
+
+
+}
